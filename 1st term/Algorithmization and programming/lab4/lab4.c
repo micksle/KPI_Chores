75705f01017cd2180c3c964dce08ef5b3923d4cf
Yaroslav Kalinin <109234959+mickzle@users.noreply.github.com>
2022-07-14 15:27:16 +0300

    Update lab4.c

--- 1st term/Algorithmization and programming/lab4/lab4.c
+++ 1st term/Algorithmization and programming/lab4/lab4.c
@@ -32,40 +32,7 @@ for(int i = 0; i < l; i++){
 return 0;
 } 
 
-int  task2(){
-int  matrix[9][9];
-printf("Initial matrix:\n\n");
-for(int i = 0; i < 9; i++){
- for(int j = 0; j < 9; j++){
-   matrix[i][j] = rand()%99;
-   printf("%.2d  ", matrix [i][j]);
- }
- printf("\n");
-}
-printf("\n\nSorted matrix:\n\n");
-  
-int N = 9 ;
-for (int i = 0; i < N/2; ++i){
- for (int j = i; j < N-i-1; ++j){
-  int temp = matrix[i][N-1-j];
-  matrix[i][N-1-j] = matrix[N-1-j][N-1-i];
-  matrix[N-1-j][N-1-i] = matrix[N-1-i][j];
-  matrix[N-1-i][j] = matrix[j][i];
-  matrix[j][i] = temp;
-   }
- }
- for(int i = 0; i < 9; i++){
-  for(int j = 0; j < 9; j++){
-   printf("%.2d  ", matrix[i][j]);
- }
- printf("\n");
-}
-return 0 ;
-}
-
 int main(void){
 printf("\t\tResults of the first task \n\n");
 task1();
-printf("\n\n\t\tResults of the second task \n\n");
-task2();
 }
