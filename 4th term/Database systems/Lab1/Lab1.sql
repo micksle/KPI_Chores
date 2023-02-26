efc25bd51e5819ce47b578346ee4ba0899cbacc7
Yaroslav Kalinin <109234959+mickzle@users.noreply.github.com>
2023-02-27 01:04:10 +0200

    Update Lab1.sql

--- 4th term/Database systems/Lab1/Lab1.sql
+++ 4th term/Database systems/Lab1/Lab1.sql
@@ -9,4 +9,18 @@ route_Price DOUBLE NOT NULL
 );
 
 INSERT INTO route (route_Name, route_Distance, route_Price) VALUES ('First route', 20.1, 70.7);
+INSERT INTO route (route_Name, route_Distance, route_Price) VALUES ('Second route', 32.3, 86.2);
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\Lab1.txt' INTO TABLE route;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE route SET route_Name='New fourth path', route_Distance = 3.8 WHERE route_id=4;
+DELETE FROM route WHERE route_id=3;
+DELETE FROM route WHERE route_id>=2;
+
+ALTER TABLE route ADD route_complexity VARCHAR(20) primary key;
+UPDATE route SET route_Distance = 23.8 WHERE route_id=1;
+UPDATE route SET route_Price =0 WHERE route_id=1;
+
+DROP TABLE route;
+DROP DATABASE Lab1;
 SELECT * FROM route;
