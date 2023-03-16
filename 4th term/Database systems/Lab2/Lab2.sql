5e68c6a532856a109b6c9b46803ed8c9241fbab9
Yaroslav Kalinin <109234959+mickzle@users.noreply.github.com>
2023-03-16 16:45:45 +0200

    Update Lab2.sql

--- 4th term/Database systems/Lab2/Lab2.sql
+++ 4th term/Database systems/Lab2/Lab2.sql
@@ -1,26 +1,27 @@
-CREATE DATABASE Lab2;
-USE Lab2;
+CREATE DATABASE lab2;
+USE lab2;
 
-CREATE TABLE transportation (
+CREATE TABLE trasnportation (
 ID INT NOT NULL AUTO_INCREMENT,
-Route VARCHAR(40) NOT NULL,
-Driver VARCHAR(10) NOT NULL,
-Departure DATETIME DEFAULT(current_timestamp()),
-Arrival DATETIME DEFAULT(current_timestamp()),
-Premium DOUBLE DEFAULT(0) CHECK (Premium < 1),
+Route INT NOT NULL,
+FirstDriver INT NOT NULL,
+SecondDriver INT,
+DepartureTime DATETIME DEFAULT(current_timestamp()),
+ArrivalTime DATETIME DEFAULT(current_timestamp()),
 
-PRIMARY KEY (`ID`)
+PRIMARY KEY (`ID`),
+CHECK (DepartureTime <= ArrivalTime),
+CONSTRAINT routeCONST FOREIGN KEY (Route) REFERENCES route(ID),
+CONSTRAINT firstDriverCONST FOREIGN KEY (FirstDriver) REFERENCES driver(ID),
+CONSTRAINT secondDriverCONST FOREIGN KEY (SecondDriver) REFERENCES driver(ID)
 );
 
-CREATE TABLE route (
+CREATE TABLE price (
 ID INT NOT NULL AUTO_INCREMENT,
-DepartureName VARCHAR(20) NOT NULL,
-ArrivalName VARCHAR(20) NOT NULL,
-Distance DOUBLE NOT NULL,
-Payment DOUBLE NOT NULL,
 
-CHECK (Distance > 0 AND Payment > 0),
-PRIMARY KEY (`ID`)
+
+CHECK (ID > 2),
+PRIMARY KEY (ID)
 );
 
 CREATE TABLE driver (
@@ -30,57 +31,108 @@ Surname VARCHAR(10) NOT NULL,
 Lastname VARCHAR(10),
 Experience DOUBLE NOT NULL,
 
-CHECK (Experience > 0),
-#PRIMARY KEY (`ID`)
-PRIMARY KEY (Name, Surname, Lastname)
+CHECK (Experience > 0 AND Experience < 45 AND LENGTH(Name) > 1 AND LENGTH(Surname) >= 1),
+PRIMARY KEY (ID)
 );
 
-CREATE TABLE payment (
+CREATE TABLE route (
 ID INT NOT NULL AUTO_INCREMENT,
-Route INT NOT NULL,
-Experience INT NOT NULL,
-Amount INT NOT NULL,
+Path INT NOT NULL,
+Distance DOUBLE NOT NULL,
+Payment DECIMAL(8,2) NOT NULL, 
 
-CHECK (Amount > 0),
-PRIMARY KEY (`ID`)
+CHECK (Distance > 0 AND Payment > 0),
+PRIMARY KEY (ID),
+CONSTRAINT pathCONST FOREIGN KEY (Path) REFERENCES path(ID)
+);
+
+CREATE TABLE path (
+ID INT NOT NULL AUTO_INCREMENT,
+DepartureName VARCHAR(20) NOT NULL,
+ArrivalName VARCHAR(20) NOT NULL,
+
+CHECK (LENGTH(DepartureName) > 2 AND LENGTH (ArrivalName) > 2),
+PRIMARY KEY (ID)
 );
 
 # -------------- -------------- -------------- DATABASE MANAGEMENT -------------- -------------- --------------
 # -------------- -------------- -------------- |-------- --------| -------------- -------------- --------------
-SELECT * FROM route;
+SELECT * FROM trasnportation;
+SELECT * FROM price;
 SELECT * FROM driver;
-SELECT * FROM transportation;
+SELECT * FROM route;
+SELECT * FROM path;
 
-DROP TABLE route;
-DROP TABLE driver;
-DROP TABLE transportation;
+DROP TABLE IF EXISTS  trasnportation;
+DROP TABLE IF EXISTS  price;
+DROP TABLE IF EXISTS  driver;
+DROP TABLE IF EXISTS  route;
+DROP TABLE IF EXISTS  path;
 
+DROP DATABASE lab2;
 # -------------- -------------- -------------- FILLING IN DATABASE -------------- -------------- --------------
 # -------------- -------------- -------------- |-------- --------| -------------- -------------- --------------
-INSERT INTO route (DepartureName, ArrivalName, Distance, Payment) VALUES ('Kyiv', 'Lviv', 541, 1349);
-INSERT INTO route (DepartureName, ArrivalName, Distance, Payment) VALUES ('Kyiv', 'Dnipro', 485, 1149);
-INSERT INTO route (DepartureName, ArrivalName, Distance, Payment) VALUES ('Kyiv', 'Odessa', 475, 1099);
-INSERT INTO route (DepartureName, ArrivalName, Distance, Payment) VALUES ('Kyiv', 'Kharkiv', 489, 1099);
-INSERT INTO route (DepartureName, ArrivalName, Distance, Payment) VALUES ('Dnipro', 'Kharkiv', 218, 699);
-INSERT INTO route (DepartureName, ArrivalName, Distance, Payment) VALUES ('Dnipro', 'Kherson', 329, 899);
-INSERT INTO route (DepartureName, ArrivalName, Distance, Payment) VALUES ('Dnipro', 'Zaporizhzhia', 85.1, 349);
-INSERT INTO route (DepartureName, ArrivalName, Distance, Payment) VALUES ('Kherson', 'Crimea', 255, 799);
-INSERT INTO route (DepartureName, ArrivalName, Distance, Payment) VALUES ('Zaporizhzhia', 'Mariupol', 227, 499);
+INSERT INTO trasnportation (Route, FirstDriver, DepartureTime, ArrivalTime) VALUES
+(1, 1,'2023-03-13 22:17:42', '2023-03-14 04:06:51'),
+(2, 2, current_timestamp(), '2023-03-18 14:22:53'),
+(4, 3, current_timestamp(), '2023-03-16 19:05:23'),
+(5, 6, '2023-03-15 13:17:17', '2023-03-16 8:07:03'),
+(6, 4, '2023-02-13 22:17:42', '2023-02-14 04:06:51'),
+(8, 8, current_timestamp(), '2023-03-18 14:22:53'),
+(9, 5, '2023-03-13 13:11:02', '2023-03-13 23:15:23'),
+(11, 7, current_timestamp(), '2023-03-16 19:05:23'),
+(13, 4, '2023-03-15 07:08:10', '2023-03-15 16:25:31');
 
-INSERT INTO driver (Name, Surname, Lastname, Experience) VALUES ('Driver', '1', '', 1);
-INSERT INTO driver (Name, Surname, Lastname, Experience) VALUES ('Driver', '2', '_', 2.2);
-INSERT INTO driver (Name, Surname, Lastname, Experience) VALUES ('Driver', '3', '', 3);
-INSERT INTO driver (Name, Surname, Lastname, Experience) VALUES ('Driver', '4', '', 4.4);
-INSERT INTO driver (Name, Surname, Lastname, Experience) VALUES ('Driver', '5', '', 5.5);
-INSERT INTO driver (Name, Surname, Lastname, Experience) VALUES ('Driver', '6', '_', 6);
+INSERT INTO trasnportation (Route, FirstDriver, SecondDriver, DepartureTime, ArrivalTime) VALUES
+(3, 1, 2, '2023-03-15 13:17:17', '2023-03-16 8:07:03'),
+(10, 3, 8, '2023-03-15 07:08:10', '2023-03-15 16:25:31'),
+(7, 3, 1, '2023-04-16 16:34:27', '2023-04-16 21:17:07'),
+(12, 5, 6, '2023-03-15 15:09:26', '2023-03-15 11:05:34');
 
-INSERT INTO transportation (Route, Driver, Departure, Arrival, Premium) VALUES ('Kyiv-Lviv', 'Driver 1', current_timestamp(), '2023-03-16 19:05:23', .15);
-INSERT INTO transportation (Route, Driver, Departure, Arrival, Premium) VALUES ('Kyiv-Lviv', 'Driver 3', current_timestamp(), '2023-03-16 19:05:23', .15);
-INSERT INTO transportation (Route, Driver, Departure, Arrival) VALUES ('Kyiv-Dnipro', 'Driver 2', '2023-03-15 13:17:17', '2023-03-16 8:07:03');
-INSERT INTO transportation (Route, Driver, Departure, Arrival) VALUES ('Dnipro-Kharkiv', 'Driver 6', '2023-03-13 22:17:42', '2023-03-14 04:06:51');
-INSERT INTO transportation (Route, Driver, Departure, Arrival) VALUES ('Kyiv-Kharkiv', 'Driver 4', '2023-03-13 13:11:02', '2023-03-13 23:15:23');
-INSERT INTO transportation (Route, Driver, Departure, Arrival) VALUES ('Zaporizhzhia-Mariupol', 'Driver 5', current_timestamp(), '2023-03-18 14:22:53');
-INSERT INTO transportation (Route, Driver, Departure, Arrival) VALUES ('Dnipro-Kherson', 'Driver 5', '2023-03-17 14:48:59', '2023-03-17 19:33:07');
-INSERT INTO transportation (Route, Driver, Departure, Arrival, Premium) VALUES ('Dnipro-Zaporizhzhia', 'Driver 2', '2023-03-15 07:08:10', '2023-03-15 16:25:31', .10);
-INSERT INTO transportation (Route, Driver, Departure, Arrival) VALUES ('Kherson-Crimea', 'Driver 3', '2023-04-16 16:34:27', '2023-04-16 21:17:07');
-INSERT INTO transportation (Route, Driver, Departure, Arrival) VALUES ('Kyiv-Odessa', 'Driver 1', '2023-03-15 15:09:26', '2023-03-15 11:05:34');
+
+INSERT INTO price () VALUES
+(),
+();
+
+
+INSERT INTO driver (Name, Surname, Lastname, Experience) VALUES
+('Driver', '1', '', 1),
+('Driver', '2', 'Vodiy', 2.2),
+('Driver', '3', 'Vairuotojas', 3.3),
+('Driver', '4', 'Vodiyev', 4.4),
+('Driver', '5', '', 5),
+('Driver', '6', 'Kierowca', 6),
+('Driver', '7', '', 7.7),
+('Driver', '8', '', 8);
+
+
+INSERT INTO route (Path, Distance, Payment) VALUES
+(1, 541, 1090.30),
+(2, 652, 1340.0),
+(3, 1050, 2080.30),
+(4, 485, 1120.90),
+(5, 744, 1580.60),
+(6, 839, 1600.0),
+(7, 1571, 2980.40),
+(8, 314, 899.90),
+(9, 681, 1470.0),
+(10, 1321, 2400.80),
+(12, 618, 1360.20),
+(11, 463, 999),
+(13, 98.2, 450.99);
+
+
+INSERT INTO path (DepartureName, ArrivalName) VALUES
+('Kyiv', 'Lviv'),
+('Odessa', 'Mariupol'),
+('Riga','Lviv'),
+('Kyiv', 'Dnipro'),
+('Kharkiv', 'Odessa'),
+('Crimea', 'Kyiv'),
+('Dnipro','Poznań'),
+('Zaporizhzhia', 'Kherson'),
+('Poltava','Rivne'),
+('Kyiv','Tallinn'),
+('Vinnitsa','Mykolaiv'),
+('Ivano-Frankivsk','Warsaw'),
+('Berdyansk','Genichesk');
