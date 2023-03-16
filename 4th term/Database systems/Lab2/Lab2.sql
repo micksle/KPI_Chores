03338d84f99b6cc6bc78ac5e5ba4e68a668bf8e7
Yaroslav Kalinin <109234959+mickzle@users.noreply.github.com>
2023-03-16 23:30:17 +0200

    Update Lab2.sql

--- 4th term/Database systems/Lab2/Lab2.sql
+++ 4th term/Database systems/Lab2/Lab2.sql
@@ -12,6 +12,15 @@ CHECK (Experience > 0 AND Experience < 45 AND LENGTH(Name) > 1 AND LENGTH(Surnam
 PRIMARY KEY (ID)
 );
 
+CREATE TABLE path (
+ID INT NOT NULL AUTO_INCREMENT,
+DepartureName VARCHAR(20) NOT NULL,
+ArrivalName VARCHAR(20) NOT NULL,
+
+CHECK (LENGTH(DepartureName) > 2 AND LENGTH (ArrivalName) > 2),
+PRIMARY KEY (ID)
+);
+
 CREATE TABLE route (
 ID INT NOT NULL AUTO_INCREMENT,
 Path INT NOT NULL,
@@ -21,15 +30,6 @@ Payment DECIMAL(8,2) NOT NULL,
 CHECK (Distance > 0 AND Payment > 0),
 PRIMARY KEY (ID),
 CONSTRAINT pathCONST FOREIGN KEY (Path) REFERENCES path(ID)
-);
-
-CREATE TABLE path (
-ID INT NOT NULL AUTO_INCREMENT,
-DepartureName VARCHAR(20) NOT NULL,
-ArrivalName VARCHAR(20) NOT NULL,
-
-CHECK (LENGTH(DepartureName) > 2 AND LENGTH (ArrivalName) > 2),
-PRIMARY KEY (ID)
 );
 
 CREATE TABLE trasnportation (
@@ -42,7 +42,7 @@ ArrivalTime DATETIME DEFAULT(current_timestamp()),
 Bonus DECIMAL(4, 2),
 
 PRIMARY KEY (`ID`),
-CHECK (DepartureTime <= ArrivalTime AND Bonus <=  100),
+CHECK (DepartureTime <= ArrivalTime AND Bonus <= 100),
 CONSTRAINT routeCONST FOREIGN KEY (Route) REFERENCES route(ID),
 CONSTRAINT firstDriverCONST FOREIGN KEY (FirstDriver) REFERENCES driver(ID),
 CONSTRAINT secondDriverCONST FOREIGN KEY (SecondDriver) REFERENCES driver(ID)
@@ -50,37 +50,38 @@ CONSTRAINT secondDriverCONST FOREIGN KEY (SecondDriver) REFERENCES driver(ID)
 
 CREATE TABLE salary (
 ID INT NOT NULL AUTO_INCREMENT,
-DriversPayment DECIMAL(8,2) NOT NULL,
+DriversSalary DECIMAL(8,2) NOT NULL,
 Route INT NOT NULL,
-Experience DOUBLE NOT NULL DEFAULT(1),
+Experience INT NOT NULL DEFAULT(1),
 SecondDriver BOOLEAN NOT NULL,
 
 PRIMARY KEY (ID),
-CONSTRAINT PaymentRouteCONST FOREIGN KEY (Route) REFERENCES route(ID),
-CONSTRAINT DriverExperienceCONST FOREIGN KEY (Experience) REFERENCES driver(ID)
+#CONSTRAINT SalaryRouteCONST FOREIGN KEY (DriversSalary) REFERENCES route(ID), #Payment
+CONSTRAINT PaymentRouteCONST FOREIGN KEY (Route) REFERENCES route(ID),  #Path
+CONSTRAINT DriverExperienceCONST FOREIGN KEY (Experience) REFERENCES driver(ID)  #Experience
 );
 
 CREATE TABLE costOfTrasnportation (
 ID INT NOT NULL AUTO_INCREMENT,
 TrasnportationCost DECIMAL(8,2) NOT NULL,
-DriversPayment INT NOT NULL,
+DriversSalary INT NOT NULL,
 
 PRIMARY KEY (ID),
-CONSTRAINT salaryCONST FOREIGN KEY (DriversPayment) REFERENCES salary(ID)
+CONSTRAINT salaryCONST FOREIGN KEY (DriversSalary) REFERENCES salary(ID)
 );
 
 # -------------- -------------- -------------- DATABASE MANAGEMENT -------------- -------------- --------------
 # -------------- -------------- -------------- |-------- --------| -------------- -------------- --------------
 SELECT * FROM driver;
-SELECT * FROM route;
 SELECT * FROM path;
+SELECT * FROM route;
 SELECT * FROM salary;
 SELECT * FROM costOfTrasnportation;
 SELECT * FROM trasnportation;
 
 DROP TABLE IF EXISTS  driver;
-DROP TABLE IF EXISTS  route;
 DROP TABLE IF EXISTS  path;
+DROP TABLE IF EXISTS  route;
 DROP TABLE IF EXISTS  salary;
 DROP TABLE IF EXISTS  costOfTrasnportation;
 DROP TABLE IF EXISTS  trasnportation;
@@ -88,7 +89,7 @@ DROP TABLE IF EXISTS  trasnportation;
 CREATE INDEX DriversExperience ON driver(Experience);
 #ALTER TABLE driver DROP INDEX DriversExperience;
 
-DROP DATABASE lab2;
+DROP DATABASE IF EXISTS lab2;
 # -------------- -------------- -------------- FILLING IN DATABASE -------------- -------------- --------------
 # -------------- -------------- -------------- |-------- --------| -------------- -------------- --------------
 INSERT INTO driver (Name, Surname, Lastname, Experience) VALUES
@@ -100,22 +101,6 @@ INSERT INTO driver (Name, Surname, Lastname, Experience) VALUES
 ('Driver', '6', 'Kierowca', 6),
 ('Driver', '7', '', 7.7),
 ('Driver', '8', '', 8);
-
-
-INSERT INTO route (Path, Distance, Payment) VALUES
-(1, 541, 1090.30),
-(2, 652, 1340.0),
-(3, 1050, 2080.30),
-(4, 485, 1120.90),
-(5, 744, 1580.60),
-(6, 839, 1600.0),
-(7, 1571, 2980.40),
-(8, 314, 899.90),
-(9, 681, 1470.0),
-(10, 1321, 2400.80),
-(12, 618, 1360.20),
-(11, 463, 999),
-(13, 98.2, 450.99);
 
 
 INSERT INTO path (DepartureName, ArrivalName) VALUES
@@ -134,7 +119,23 @@ INSERT INTO path (DepartureName, ArrivalName) VALUES
 ('Berdyansk','Genichesk');
 
 
-INSERT INTO salary (Route, DriversPayment, Experience, SecondDriver) VALUES
+INSERT INTO route (Path, Distance, Payment) VALUES
+(1, 541, 1090.30),
+(2, 652, 1340.0),
+(3, 1050, 2080.30),
+(4, 485, 1120.90),
+(5, 744, 1580.60),
+(6, 839, 1600.0),
+(7, 1571, 2980.40),
+(8, 314, 899.90),
+(9, 681, 1470.0),
+(10, 1321, 2400.80),
+(12, 618, 1360.20),
+(11, 463, 999),
+(13, 98.2, 450.99);
+
+
+INSERT INTO salary (Route, DriversSalary, Experience, SecondDriver) VALUES
 (1, 1090.30, 1, false),
 (2, 1340.0, 2, false),
 (3, 2080.30, 3, true),
@@ -143,27 +144,27 @@ INSERT INTO salary (Route, DriversPayment, Experience, SecondDriver) VALUES
 (6, 1600.0, 6, false);
 
 
-INSERT INTO costOfTrasnportation (TrasnportationCost, DriversPayment) VALUES
-(1240.9, 1),
-(1308.10, 1),
-(1490.8, 3);
-
-
 INSERT INTO trasnportation (Route, FirstDriver, DepartureTime, ArrivalTime) VALUES
 (1, 1,'2023-03-13 22:17:42', '2023-03-14 04:06:51'),
-(2, 2, current_timestamp(), '2023-03-18 14:22:53'),
-(4, 3, current_timestamp(), '2023-03-16 19:05:23'),
+(2, 2, current_timestamp(), '2023-04-18 14:22:53'),
+(4, 3, current_timestamp(), '2023-04-16 19:05:23'),
 (5, 6, '2023-03-15 13:17:17', '2023-03-16 8:07:03'),
 (9, 5, '2023-03-13 13:11:02', '2023-03-13 23:15:23'),
-(11, 7, current_timestamp(), '2023-03-16 19:05:23'),
+(11, 7, current_timestamp(), '2023-04-16 19:05:23'),
 (13, 4, '2023-03-15 07:08:10', '2023-03-15 16:25:31');
 
 INSERT INTO trasnportation (Route, FirstDriver, DepartureTime, ArrivalTime, Bonus) VALUES
 (6, 4, '2023-02-13 22:17:42', '2023-02-14 04:06:51', 9.4),
-(8, 8, current_timestamp(), '2023-03-18 14:22:53', 3.6);
+(8, 8, current_timestamp(), '2023-04-18 14:22:53', 3.6);
 
 INSERT INTO trasnportation (Route, FirstDriver, SecondDriver, DepartureTime, ArrivalTime) VALUES
 (3, 1, 2, '2023-03-15 13:17:17', '2023-03-16 8:07:03'),
 (10, 3, 8, '2023-03-15 07:08:10', '2023-03-15 16:25:31'),
 (7, 3, 1, '2023-04-16 16:34:27', '2023-04-16 21:17:07'),
 (12, 5, 6, '2023-03-15 15:09:26', '2023-03-15 19:05:34');
+
+
+INSERT INTO costOfTrasnportation (TrasnportationCost, DriversSalary) VALUES
+(1240.9, 1),
+(1308.10, 1),
+(1490.8, 3);
