b83fcce7f48977181df77fe77603d6e6b8aa8801
Yaroslav Kalinin <109234959+mickzle@users.noreply.github.com>
2023-04-04 23:52:07 +0300

    Create lab2.asm

---
+++ 2nd term/Operating systems/lab2/lab2.asm
@@ -0,0 +1,22 @@
+title lab2
+
+.model small
+
+text segment
+	assume CS:text, DS:data 
+
+body:   mov AX, data
+	mov DS, AX
+	mov AH, 09h
+	mov DX, offset mesg
+	int 21h
+	mov AH, 4Ch
+	mov Al, 0
+	int 21h
+text ends
+
+data segment
+	message db "trying to solve the lab $"
+data ends 
+
+end body
