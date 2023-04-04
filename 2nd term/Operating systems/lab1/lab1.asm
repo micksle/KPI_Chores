c68830d3124a8afe85a5622e3d3f5b944a14fb7e
Yaroslav Kalinin <109234959+mickzle@users.noreply.github.com>
2023-04-04 23:51:46 +0300

    Create lab1.asm

---
+++ 2nd term/Operating systems/lab1/lab1.asm
@@ -0,0 +1,29 @@
+.MODEL tiny
+.DATA
+
+i db 0, 255
+i1 db -255
+i2 db -128,127
+
+b1 db 150
+b2 db 122
+b3 db -150
+b4 db -122
+
+j1 dw -255
+j2 dw 0, 65535
+j3 dw -1, -32768
+j4 dw 1, 32767
+j5 dw -65535
+j6 dd -65535
+
+w1 dw 1739
+w2 dw 1711
+w3 dw -1739
+w4 dw -1711
+w5 dw 150
+w6 dw 122
+w7 dw -150
+w8 dw -122
+
+end
